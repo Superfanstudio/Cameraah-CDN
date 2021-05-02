92d76a3a1559f1b4c3334d91e5ad04d38e3e71dd
ayesha1204 <ayesha@superfan.studio>
2021-05-02 23:12:05 +0530

    chore: cleanup

--- webflow/model_try_it.js
+++ webflow/model_try_it.js
@@ -1,6 +1,7 @@
 const baseURL = "https://cameraah-labs.s3.ap-south-1.amazonaws.com/Website+assets/";
 const COLOR = 0;
 const TEXTURE = 1;
+var canvas = document.createElement('mv-canvas');
 function updateModel(model, type, modelComponent, newValue) {
     switch (type) {
     case COLOR:
@@ -20,19 +21,19 @@ function addonClick(selector, callback) {
 
 function handleCycleColor() {
     let cycle = document.querySelector('#cycle-viewer');
-    addonClick('#red-color-btn', () => { updateModel(cycle, COLOR, 'red body', 'red') });
-    addonClick('#yellow-color-btn', () => { updateModel(cycle, COLOR, 'red body', 'yellow') });
-    addonClick('#green-color-btn', () => { updateModel(cycle, COLOR, 'red body', '#00ff2f') });
+    addonClick('#red-color-btn', () => updateModel(cycle, COLOR, 'red body', 'red'));
+    addonClick('#yellow-color-btn', () => updateModel(cycle, COLOR, 'red body', 'yellow'));
+    addonClick('#green-color-btn', () => updateModel(cycle, COLOR, 'red body', '#00ff2f'));
 
-    addonClick('#black-handle-color-btn', () => { updateModel(cycle, COLOR, 'black metal', 'black') });
-    addonClick('#white-handle-color-btn', () => { updateModel(cycle, COLOR, 'black metal', 'white') });
-    addonClick('#grey-handle-color-btn', () => { updateModel(cycle, COLOR, 'black metal', 'grey') });
+    addonClick('#black-handle-color-btn', () => updateModel(cycle, COLOR, 'black metal', 'black'));
+    addonClick('#white-handle-color-btn', () => updateModel(cycle, COLOR, 'black metal', 'white'));
+    addonClick('#grey-handle-color-btn', () => updateModel(cycle, COLOR, 'black metal', 'grey'));
 
 }
 function handlePhoneCase() {
     let phone = document.querySelector('#case-viewer');
-    addonClick('#upload-texture-btn', () => { $('#case-texture').click() });
-    $('#case-texture').on('change', e => {
+    addonClick('#upload-texture-btn', () => $('#case-texture').click());
+    $('#case-texture').change( e => {
         let file = e.target.files[0];
         updateModel(phone, TEXTURE, 'Image MAT', URL.createObjectURL(file));
         $('#upload-texture-btn').css('background-image', `url("${URL.createObjectURL(file)}")`);
@@ -40,24 +41,22 @@ function handlePhoneCase() {
 }
 function handleCouch() {
     let couch = document.querySelector('#couch-viewer');
-    addonClick('#couch-metallic', () => { convertImgToBase64URL(baseURL + 'couch-texture-1.jpg', (url) => updateModel(couch, TEXTURE, 'Leather', url)) });
-    addonClick('#couch-tiger', () => { convertImgToBase64URL(baseURL + 'couch-texture-2.jpg', (url) => updateModel(couch, TEXTURE, 'Leather', url)) });
-    addonClick('#couch-wood', () => { convertImgToBase64URL(baseURL + 'couch-texture-3.jpg', (url) => updateModel(couch, TEXTURE, 'Leather', url)) });
+    addonClick('#couch-metallic', () => convertImgToBase64URL(baseURL + 'couch-texture-1.jpg', (url) => updateModel(couch, TEXTURE, 'Leather', url)));
+    addonClick('#couch-tiger', () => convertImgToBase64URL(baseURL + 'couch-texture-2.jpg', (url) => updateModel(couch, TEXTURE, 'Leather', url)));
+    addonClick('#couch-wood', () => convertImgToBase64URL(baseURL + 'couch-texture-3.jpg', (url) => updateModel(couch, TEXTURE, 'Leather', url)));
 
-    addonClick('#pillow-grey', () => { convertImgToBase64URL(baseURL + 'pillow-texture-1.jpg', (url) => updateModel(couch, TEXTURE, 'Pillow1', url)) });
-    addonClick('#pillow-fun', () => { convertImgToBase64URL(baseURL + 'pillow-texture-2.jpg', (url) => updateModel(couch, TEXTURE, 'Pillow1', url)) });
-    addonClick('#pillow-red', () => { convertImgToBase64URL(baseURL + 'pillow-texture-3.jpg', (url) => updateModel(couch, TEXTURE, 'Pillow1', url)) });
+    addonClick('#pillow-grey', () => convertImgToBase64URL(baseURL + 'pillow-texture-1.jpg', (url) => updateModel(couch, TEXTURE, 'Pillow1', url)));
+    addonClick('#pillow-fun', () => convertImgToBase64URL(baseURL + 'pillow-texture-2.jpg', (url) => updateModel(couch, TEXTURE, 'Pillow1', url)));
+    addonClick('#pillow-red', () => convertImgToBase64URL(baseURL + 'pillow-texture-3.jpg', (url) => updateModel(couch, TEXTURE, 'Pillow1', url)));
 }
 
 handleCycleColor();
 handlePhoneCase();
 handleCouch();
 
-function convertImgToBase64URL(url, callback){
-    var img = new Image();
-    img.crossOrigin = 'anonymous';
-    img.onload = function(){
-        var canvas = document.createElement('CANVAS'),
+function convertImgToBase64URL(url, callback) {
+    let img = new Image();
+    img.onload = function() {
         ctx = canvas.getContext('2d'), dataURL;
         canvas.height = img.height;
         canvas.width = img.width;
@@ -66,5 +65,6 @@ function convertImgToBase64URL(url, callback){
         callback(URL.createObjectURL(dataURL));
         canvas = null; 
     };
+    img.crossOrigin = 'anonymous';
     img.src = url;
 }
